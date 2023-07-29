cbd59ba4e50cae7c7c0ba38ad3ac416a5232103b
prajjwal <prajjwaltripathi174@gmail.com>
2023-07-29 14:35:08 +0530

    runAsync() method

--- src/main/java/com/tripathiji/Main.java
+++ src/main/java/com/tripathiji/Main.java
@@ -7,18 +7,25 @@ public class Main {
         System.out.println("Main Thread starts { ");
         System.out.println(" Network Process Starts...");
 
-        //The solution to this problem is  to make the network method a 'CompletableFuture'
-        //meaning that a process that will be done sometime in the future, but for that it
-        //doesn't need to block other method in the main thread, or in the current thread in which
-        //it is called.
+//        //The solution to this problem is  to make the network method a 'CompletableFuture'
+//        //meaning that a process that will be done sometime in the future, but for that it
+//        //doesn't need to block other method in the main thread, or in the current thread in which
+//        //it is called.
+//
+//        CompletableFuture.supplyAsync(()-> networkProcess(5))
+//                .thenAccept(value -> System.out.println("network output received : "+value));
+//        //in this case if you try to run the program the whole program will end immediately as, the whole
+//        //program finishes (that contains the network process) before the network process finishes it's work,
+//        //in case of industrial application the application keeps running on the server hence waiting for the
+//        // output that a network process provides.
+//        //to stimulate above conditions, let's make the main thread sleep(delay ending) for 6 seconds i.e. it will end three
+//
+//
+        //but if suppose you have a network process that is very long and doesn't give you an output,
+        //for example you are uploading a file to a server. in that case instead of using 'supplyAsync()'
+        //we use "runAsync()" method of CompletableFuture.
 
-        CompletableFuture.supplyAsync(()-> networkProcess(5))
-                .thenAccept(value -> System.out.println("network output received : "+value));
-        //in this case if you try to run the program the whole program will end immediately as, the whole
-        //program finishes (that contains the network process) before the network process finishes it's work,
-        //in case of industrial application the application keeps running on the server hence waiting for the
-        // output that a network process provides.
-        //to stimulate above conditions, let's make the main thread sleep(delay ending) for 6 seconds i.e. it will end three
+        CompletableFuture.runAsync(()-> veryLongProcess());
 
         System.out.println(" Another process A");
         System.out.println(" Another process B");
@@ -31,6 +38,11 @@ public class Main {
 
         System.out.println("} Main thread ends");
 
+    }
+
+    public static void veryLongProcess(){
+        sleepForAWhile(4);
+        System.out.println("Very long process execution completed!!!!!");
     }
 
     public static int networkProcess(int num){
