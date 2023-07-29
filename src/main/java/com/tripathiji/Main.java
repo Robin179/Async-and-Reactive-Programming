48bca578d0c3dcf123560dc065ea60362ce33b7b
prajjwal <prajjwaltripathi174@gmail.com>
2023-07-29 14:27:57 +0530

    supplyAsync() method

--- src/main/java/com/tripathiji/Main.java
+++ src/main/java/com/tripathiji/Main.java
@@ -1,34 +1,49 @@
 package com.tripathiji;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main Thread starts { ");
         System.out.println(" Network Process Starts...");
-        System.out.println(networkProcess(5));
-        //when we call a network process (on a server) it is obvious that its time of response is not fixed,
-        //it may provide us with desired resource very quickly or it takes some time.
-        //here, i've tried to simulate a similar situation
-        //in this case unless and until the network process completes, no other process after it could run
-        //that may be independent of output of this network process
 
-        //The main thread gets 'blocked' in this way making the application a 'synchronized' one,
+        //The solution to this problem is  to make the network method a 'CompletableFuture'
+        //meaning that a process that will be done sometime in the future, but for that it
+        //doesn't need to block other method in the main thread, or in the current thread in which
+        //it is called.
+
+        CompletableFuture.supplyAsync(()-> networkProcess(5))
+                .thenAccept(value -> System.out.println("network output received : "+value));
+        //in this case if you try to run the program the whole program will end immediately as, the whole
+        //program finishes (that contains the network process) before the network process finishes it's work,
+        //in case of industrial application the application keeps running on the server hence waiting for the
+        // output that a network process provides.
+        //to stimulate above conditions, let's make the main thread sleep(delay ending) for 6 seconds i.e. it will end three
 
         System.out.println(" Another process A");
         System.out.println(" Another process B");
         System.out.println(" All processes finish");
+
+
+        System.out.println("delaying main thread to wait for network process response");
+        sleepForAWhile(6);
+
 
         System.out.println("} Main thread ends");
 
     }
 
     public static int networkProcess(int num){
-       try{
-           Thread.sleep(3000);
-       }
-       catch (Exception e){
-           System.out.println(e.getCause()+" "+e.getMessage());
-       }
-
+       sleepForAWhile(3);
        return num * 10;
+    }
+
+    public static void sleepForAWhile(int sec){
+        try{
+            Thread.sleep(sec* 1000L);
+        }
+        catch (Exception e){
+            System.out.println(e.getCause()+" "+e.getMessage());
+        }
     }
 }
